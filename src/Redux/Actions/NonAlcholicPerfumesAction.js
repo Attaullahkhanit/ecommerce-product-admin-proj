867c5a258cc6a8d2b86a6c5bad454b1a13a58262
Atta Khan <35913737+Attaullahkhanit@users.noreply.github.com>
2023-03-22 16:45:15 +0500

    Update NonAlcholicPerfumesAction.js

--- src/Redux/Actions/NonAlcholicPerfumesAction.js
+++ src/Redux/Actions/NonAlcholicPerfumesAction.js
@@ -1,7 +1,9 @@
 
+const BaseUrl = process.env.REACT_APP_BASE_URL
+
 export const nonAlcholicPerfumesAction = (objdata) => {
     return function (dispatch) {
-        fetch('https://backend-apis.pasha.org.uk/all-none-alcohlic-perfume',)
+        fetch('${BaseUrl}/all-none-alcohlic-perfume',)
             .then((response) => response.json())
             .then((res) => {
                 dispatch({
