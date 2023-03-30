955a3b5c34c4158255c2bfb0a2b8e4dc9cc27ad2
Atta Khan <35913737+Attaullahkhanit@users.noreply.github.com>
2023-03-30 16:26:07 +0500

    Update RegUsersListDetail.js

--- src/Pages/Registered Users List/RegUsersListDetail.js
+++ src/Pages/Registered Users List/RegUsersListDetail.js
@@ -7,6 +7,7 @@ import { Suspense } from 'react';
 import LinearProgress from '@mui/material/LinearProgress';
 
 const Dashboard = React.lazy(() => import('../Dashboard/Dashboard'));
+const baseUrl = process.env.REACT_APP_BASEURL;
 
 function RegUsersListDetail() {
   const navigate = useNavigate();
