e0a09bbebad4630b795f15df18bc2928674ebbf9
Atta Khan <35913737+Attaullahkhanit@users.noreply.github.com>
2023-03-30 16:08:21 +0500

    Update AlcholicPerfumesUpdate.js

--- src/Pages/AlcholicPerfumes/AlcholicPerfumesUpdate.js
+++ src/Pages/AlcholicPerfumes/AlcholicPerfumesUpdate.js
@@ -11,6 +11,7 @@ import { Suspense } from 'react';
 import LinearProgress from '@mui/material/LinearProgress';
 
 const Dashboard = React.lazy(() => import('../Dashboard/Dashboard'));
+const baseUrl = process.env.REACT_APP_BASEURL;
 
 function AlcholicPerfumesUpdate() {
   const locationData = useLocation();
@@ -66,7 +67,7 @@ function AlcholicPerfumesUpdate() {
       }),
     };
 
-    fetch(`https://backend-apis.pasha.org.uk/update-alcohlic-perfume/${locationData?.state?._id}`, requestOptions)
+    fetch(`${baseUrl}/update-alcohlic-perfume/${locationData?.state?._id}`, requestOptions)
       .then(response => response.json())
       .then(result => {
         console.log(result, 'updated result')
